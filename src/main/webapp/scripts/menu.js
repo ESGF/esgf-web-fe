dbe6d4a22b5b6ddf3954b28f6f1ae3b2c67ebf8b
John Harney <harneyjf@ornl.gov>
2010-10-25 19:23:51 -0400

    Commented out a section for debugging...not sure about the nature of the use of this file...

--- src/main/webapp/scripts/menu.js
+++ src/main/webapp/scripts/menu.js
@@ -1,99 +1,109 @@
 /* 
-Simple JQuery menu.
 HTML structure to use:
 
-Notes: 
-
-Each menu MUST have a class 'menu' set. If the menu doesn't have this, the JS won't make it dynamic
-If you want a panel to be expanded at page load, give the containing LI element the classname 'expand'.
-Use this to set the right state in your page (generation) code.
-
-Optional extra classnames for the UL element that holds an accordion:
-
-noaccordion : no accordion functionality
-collapsible : menu works like an accordion but can be fully collapsed
-
-<ul class="menu [optional class] [optional class]">
-<li><a href="#">Sub menu heading</a>
-<ul>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-...
-...
-</ul>
-// This item is open at page load time
-<li class="expand"><a href="#">Sub menu heading</a>
-<ul>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-...
-...
-</ul>
-...
-...
-</ul>
+	<ul class="menu optional">
+	<li> 
+		<a href="#"> sub menu </a>
+		<ul>
+			<li><a href="..."> link </a></li>
+		</ul>
+	</li>
+	...
+	</ul>
+	
 
 Copyright 2007-2010 by Marco van Hylckama Vlieg
 
-web: http://www.i-marco.nl/weblog/
-email: marco@i-marco.nl
+Extended - by fwang2
 
-Free to use any way you like.
+	- disabled class for sub-menu
+	- onclick event for subfacet
+
 */
 
 
 jQuery.fn.initMenu = function() {  
+	//alert("initMenu");
     return this.each(function(){
         var theMenu = $(this).get(0);
-        $('.acitem', this).hide();
-        $('li.expand > .acitem', this).show();
-        $('li.expand > .acitem', this).prev().addClass('active');
-        $('li a', this).click(
-            function(e) {
-                e.stopImmediatePropagation();
-                var theElement = $(this).next();
-                var parent = this.parentNode.parentNode;
-                if($(parent).hasClass('noaccordion')) {
-                    if(theElement[0] === undefined) {
-                        window.location.href = this.href;
-                    }
-                    $(theElement).slideToggle('normal', function() {
-                        if ($(this).is(':visible')) {
-                            $(this).prev().addClass('active');
-                        }
-                        else {
-                            $(this).prev().removeClass('active');
-                        }    
-                    });
-                    return false;
+        $('.acitem:not(:has(li.selected))').hide();
+        $('li:has(li.selected)', this).addClass("expand");
+        $('li.expand').show();
+        
+        
+        $('ul.acitem > li > a').bind('click', function(e) {
+        	
+        	/*
+        	var facetkey = $("#entry_key", this).html().trim();
+        	var facetvalue = $("#subfacet_label", this).html().trim();
+        	//var queryString= facetkey + '=' + facetvalue + '&offset=0' + 
+        	//		'&text='+ '&limit=10';
+        	//var queryString = 'project:EOSDIS';
+        	var queryString = 'west_degrees' + '=' + -111.0;
+        	alert("pre queryString: " + queryString);
+            $.ajax({
+        		type: "POST",
+        		//url: '/esg-web/search.htm',
+        		url: 'http://localhost:8983/solr',
+        		data: queryString,
+        		success: function(data) {
+        			//alert("success");
+        			//alert("data\n   " + data);
+        			$("#search_results").html(data);
+        		}
+        	});
+            alert("post queryString: " + queryString);
+            */
+        	return true;
+        });
+        
+        $('ul.menu > li > a').bind('click', function(e) {
+            
+            e.stopImmediatePropagation();
+            var theElement = $(this).next();
+            var parent = this.parentNode.parentNode;
+            if ($(parent).hasClass('noaccordion')) 
+            {
+                if (theElement[0] === undefined) {
+                    window.location.href = this.href;
                 }
-                else {
-                    if(theElement.hasClass('acitem') && theElement.is(':visible')) {
-                        if($(parent).hasClass('collapsible')) {
-                            $('.acitem:visible', parent).first().slideUp('normal', 
-                            function() {
-                                $(this).prev().removeClass('active');
-                            }
-                        );
+                $(theElement).slideToggle('fast', function() {
+                    if ($(this).is(':visible')) {
+                        $(this).prev().addClass('active');
+                    }
+                    else {
+                        $(this).prev().removeClass('active');
+                    }    
+                });
+                return false;
+            }
+            else 
+            { // with accordion
+                if (theElement.hasClass('acitem') && theElement.is(':visible')) 
+                {
+                    if($(parent).hasClass('collapsible')) {
+                        $('.acitem:visible', parent).first().slideUp('normal', 
+                        function() {
+                            $(this).prev().removeClass('active');
+                        });
                         return false;  
                     }
                     return false;
                 }
-                if(theElement.hasClass('acitem') && !theElement.is(':visible')) {         
+                
+                if(theElement.hasClass('acitem') && !theElement.is(':visible')) 
+                {
                     $('.acitem:visible', parent).first().slideUp('normal', function() {
-                        $(this).prev().removeClass('active');
+                         $(this).prev().removeClass('active');
                     });
+                    
                     theElement.slideDown('normal', function() {
-                        $(this).prev().addClass('active');
+                         $(this).prev().addClass('active');
                     });
                     return false;
                 }
             }
-        }
-    );
-});
+    	}); 
+    });
 };
-
 $(document).ready(function() {$('.menu').initMenu();});

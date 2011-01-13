f298e81939e580e50fc167d829d2729585f5ae05
John Harney <harneyjf@ornl.gov>
2011-01-13 16:47:03 -0500

    Implemented xml parser in Meta...Controller.java
    This will increase the speed in which metadata will be accessed by the metadata overlay widget.  It takes only a subset of the metadata record and returns the subset in json form (note only OAI can be parsed).  Important assumption here is that the OAI metadata file is stored in the local temporary directory.

--- src/java/main/org/esgf/metadata/MetadataExtractorController.java
+++ src/java/main/org/esgf/metadata/MetadataExtractorController.java
@@ -7,9 +7,17 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.ws.http.HTTPException;
 
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
@@ -24,6 +32,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.apache.log4j.Logger;
 import org.esgf.solr.proxy.SolrProxyController;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+
+import esg.search.utils.XmlParser;
 
 @Controller
 @RequestMapping("/metadataproxy")
@@ -33,18 +49,18 @@ public class MetadataExtractorController {
     private final static Logger LOG = Logger.getLogger(SolrProxyController.class);
     
     //hard coded for testing - remove when finished
-    private final static String METADATA_FILE_LOCATION = System.getProperty("java.io.tmpdir");//"C:\\Users\\8xo\\esgProjects\\esgsearch12-20\\esgf-web-fe\\src\\java\\main\\org\\esgf\\solr\\proxy\\";
-    private final static String METADATA_FILE = "ORNL-oai_dif";//"C:\\Users\\8xo\\esgProjects\\esgsearch12-20\\esgf-web-fe\\src\\java\\main\\org\\esgf\\solr\\proxy\\";
+    private final static String METADATA_FILE_LOCATION = System.getProperty("java.io.tmpdir");
+    private final static String METADATA_FILE = "ORNL-oai_dif";
     
     
     @RequestMapping(method=RequestMethod.GET)
-    public @ResponseBody String doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException {
+    public @ResponseBody String doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException, ParserConfigurationException {
         LOG.debug("doGet metadataproxy");
         return relay(request, response);
     }
     
     @RequestMapping(method=RequestMethod.POST)
-    public @ResponseBody String doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException {
+    public @ResponseBody String doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException, ParserConfigurationException {
         LOG.debug("doPost");
         return relay(request, response);
     }
@@ -54,10 +70,13 @@ public class MetadataExtractorController {
      * This method will be changed soon - just used for testing
      * The metadata files should be accessed via some RESTful architecture
      */
-    private String relay(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException {
+    private String relay(HttpServletRequest request, HttpServletResponse response) throws IOException, JSONException, ParserConfigurationException {
         
         String queryString = request.getQueryString();        
         LOG.debug("queryString=" + queryString);
+        
+        
+        
         String requestUri = request.getRequestURI();
         LOG.debug("requestUri=" + requestUri);
         
@@ -65,19 +84,20 @@ public class MetadataExtractorController {
         
         String responseBody = null;
        
+        String id = request.getParameter("id");
         
         
-        String xml = "";
         
         //String metadataFileName = "ORNL-oai_dif.xml";
+        
+        
+        //Assuming the data structure is json
         
         LOG.debug("curr: " + METADATA_FILE_LOCATION);
         //File f = new File("C:\\Users\\8xo\\esgProjects\\esgsearch\\esg-search\\resources\\ORNL-oai_dif.xml");
         File f = new File(METADATA_FILE_LOCATION + METADATA_FILE + ".xml");
+        /*
         
-        
-        LOG.debug(f.getName() + " " + f.getAbsolutePath() + " " + f.getCanonicalPath());
-        //C:\Users\8xo\esgProjects
         FileReader fr = new FileReader(f);
         BufferedReader br = new BufferedReader(fr);
 
@@ -90,24 +110,159 @@ public class MetadataExtractorController {
           eachLine = br.readLine();
         }
         
-        xml = sb.toString();
-        
-        JSONObject jo = XML.toJSONObject(xml);
-        //System.out.println(jo.toString());
+        LOG.debug(sb.toString());
+        */
         
         
-        FileOutputStream out; // declare a file output object
-        PrintStream p; // declare a print stream object
+        //C:\Users\8xo\esgProjects
+        /*
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
 
-        LOG.debug("\njson\n"+jo.toString());
+        StringBuffer sb = new StringBuffer();
+        String eachLine = br.readLine();
+
+        while (eachLine != null) {
+          //sb.append(eachLine);
+          //sb.append("\n");
+          eachLine = br.readLine();
+          //LOG.debug("eachLIne: " + eachLine);
+        }
+        */
+        
+        SAXBuilder builder = new SAXBuilder();
+        //File xmlFile = new File("c:\\file.xml");
+ 
+        Element returnedEl = null;
+        String xmlContent = "";
+        try{
+ 
+           Document document = (Document) builder.build(f);
+           Element rootNode = document.getRootElement();
+           Namespace ns = (Namespace)rootNode.getNamespace();
+           LOG.debug("Successful " + rootNode.getName());
+           Element el = (Element) rootNode.getChild("ListRecords",ns);
+           LOG.debug("el " + el.getName());
+           
+           List records = (List)el.getChildren("record", ns);
+           for(int i=0;i<records.size();i++)
+           {
+               Element recordEl = (Element) records.get(i);
+               
+               Element metadataEl = (Element)recordEl.getChild("metadata",ns);
+               if(metadataEl != null)
+               {
+                   Element difEl = (Element)metadataEl.getChild("DIF",ns);
+                   if(difEl != null)
+                   {
+                       Element idEl = (Element)difEl.getChild("Entry_ID",ns);
+                       if(idEl !=null)
+                       {
+                           if(idEl.getText().equals(id))
+                           {
+                               LOG.debug("ID: " + idEl.getText());
+                           
+                               returnedEl = recordEl;
+                           }
+                       }
+                   }
+               }
+           }
+           
+           if(returnedEl == null)
+           {
+               LOG.debug("Found no element match");
+           }
+           else
+           {
+               XMLOutputter outputter = new XMLOutputter();
+               xmlContent = outputter.outputString(returnedEl);
+           }
+           
+           LOG.debug(records.size());
+          /* for (int i=0; i< list.size(); i++)
+           {
+             Element node = (Element) list.get(i);
+             LOG.debug("Name: " + node.getName());
+           }*/
+ 
+         }catch(IOException io){
+            System.out.println(io.getMessage());
+         }catch(JDOMException jdomex){
+            System.out.println(jdomex.getMessage());
+        }
+         
+        //LOG.debug("xml: \n" + xmlContent);
+        
+        JSONObject jo = XML.toJSONObject(xmlContent);
+         
+        String jsonContent = jo.toString();
+        LOG.debug("json: \n" + jo.toString());
         
         
         //String result = 
-        //responseBody = "{\"menu\": {\"id\": \"file\", \"value\": \"File\", \"popup\": { \"menuitem\": [{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},{\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},{\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}]}}}";
-        responseBody = jo.toString();    
+        responseBody = "{\"menu\": {\"id\": \"file\", \"value\": \"File\", \"popup\": { \"menuitem\": [{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},{\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},{\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}]}}}";
+        //responseBody = jo.toString();    
+        //responseBody = sb.toString();
+        
+        
         
         LOG.debug("Solr URL = " + urlString);
-        LOG.debug("responseBody = " + responseBody);
-        return responseBody;
+        LOG.debug("responseBody = " + responseBody); 
+        LOG.debug("queryString=" + queryString);
+        LOG.debug("Parameter names: " + request.getParameterNames().toString());
+        
+        Enumeration paramNames = request.getParameterNames();
+        
+        while(paramNames.hasMoreElements()) {
+          String paramName = (String)paramNames.nextElement();
+          String paramValue = request.getParameter(paramName);
+          LOG.debug(paramName + " " + paramValue);
+        }
+        return jsonContent;
     }
 }
+
+
+/*
+//File file = new File("c:\\MyXMLFile.xml");
+DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+DocumentBuilder db = dbf.newDocumentBuilder();
+
+Document doc = (Document) db.parse(f);
+Element rootEl = (Element)doc.getDocumentElement();
+
+rootEl.get
+*/
+//final Element rootEl = doc.getRootElement();
+//final Namespace ns = rootEl.getNamespace();
+//LOG.debug("Root element " + rootEl.getName());
+//
+//Element listRecordsEl = (Element)rootEl.getChild("ListRecords");
+//List records = listRecordsEl.getChildren("record");
+//
+//Iterator iterator = records.iterator();
+//
+//while(iterator.hasNext())
+//{
+//    Element record = (Element)iterator.next();
+//    LOG.debug("record" + record.getContentSize());
+//}
+
+  // NodeList nodeLst = doc.getElementsByTagName("record");
+
+
+/*FileOutputStream out; // declare a file output object
+PrintStream p; // declare a print stream object
+*/
+
+//System.out.println("Information of all employees");
+
+//xml = sb.toString();
+
+//LOG.debug(xml);
+
+//JSONObject jo = XML.toJSONObject(xml);
+//System.out.println(jo.toString());
+
+//LOG.debug("\njson\n"+jo.toString());

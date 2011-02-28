e952d7f35d79604043c8386dc9ec7c1eb64b98f0
Feiyi Wang <fwang2@ornl.gov>
2011-02-28 11:57:39 -0500

    add Dao generics

---
+++ src/java/main/org/esgf/dao/GenericDao.java
@@ -0,0 +1,25 @@
+package org.esgf.dao;
+
+/**
+ * Author: fwang2@ornl.gov
+ */
+
+import java.util.List;
+
+import org.esgf.domain.DomainObject;
+
+public interface GenericDao<T extends DomainObject> {
+
+    public T get(Long id);
+
+    public List<T> getAll();
+
+    public void save(T object);
+
+    public void delete(T object);
+
+    public void indexEntity(T object);
+
+    public void indexAllItems();
+
+}

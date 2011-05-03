19baac27f6ff76a5b8a5e641bb7cd7ba1c74c5c3
John Harney <harneyjf@ornl.gov>
2011-05-02 22:35:03 -0400

    Added licenses to the front end widgets

--- web/scripts/ajax-solr/widgets/FacetSidebar.js
+++ web/scripts/ajax-solr/widgets/FacetSidebar.js
@@ -1,3 +1,63 @@
+/*****************************************************************************
+ * Copyright © 2011 , UT-Battelle, LLC All rights reserved
+ *
+ * OPEN SOURCE LICENSE
+ *
+ * Subject to the conditions of this License, UT-Battelle, LLC (the
+ * “Licensor”) hereby grants to any person (the “Licensee”) obtaining a copy
+ * of this software and associated documentation files (the "Software"), a
+ * perpetual, worldwide, non-exclusive, irrevocable copyright license to use,
+ * copy, modify, merge, publish, distribute, and/or sublicense copies of the
+ * Software.
+ *
+ * 1. Redistributions of Software must retain the above open source license
+ * grant, copyright and license notices, this list of conditions, and the
+ * disclaimer listed below.  Changes or modifications to, or derivative works
+ * of the Software must be noted with comments and the contributor and
+ * organization’s name.  If the Software is protected by a proprietary
+ * trademark owned by Licensor or the Department of Energy, then derivative
+ * works of the Software may not be distributed using the trademark without
+ * the prior written approval of the trademark owner.
+ *
+ * 2. Neither the names of Licensor nor the Department of Energy may be used
+ * to endorse or promote products derived from this Software without their
+ * specific prior written permission.
+ *
+ * 3. The Software, with or without modification, must include the following
+ * acknowledgment:
+ *
+ *    "This product includes software produced by UT-Battelle, LLC under
+ *    Contract No. DE-AC05-00OR22725 with the Department of Energy.”
+ *
+ * 4. Licensee is authorized to commercialize its derivative works of the
+ * Software.  All derivative works of the Software must include paragraphs 1,
+ * 2, and 3 above, and the DISCLAIMER below.
+ *
+ *
+ * DISCLAIMER
+ *
+ * UT-Battelle, LLC AND THE GOVERNMENT MAKE NO REPRESENTATIONS AND DISCLAIM
+ * ALL WARRANTIES, BOTH EXPRESSED AND IMPLIED.  THERE ARE NO EXPRESS OR
+ * IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+ * OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY PATENT, COPYRIGHT,
+ * TRADEMARK, OR OTHER PROPRIETARY RIGHTS, OR THAT THE SOFTWARE WILL
+ * ACCOMPLISH THE INTENDED RESULTS OR THAT THE SOFTWARE OR ITS USE WILL NOT
+ * RESULT IN INJURY OR DAMAGE.  The user assumes responsibility for all
+ * liabilities, penalties, fines, claims, causes of action, and costs and
+ * expenses, caused by, resulting from or arising out of, in whole or in part
+ * the use, storage or disposal of the SOFTWARE.
+ *
+ *
+ ******************************************************************************/
+
+/**
+ * Facet Sidebar component for ESG
+ *
+ * fwang2@ornl.gov
+ * harneyjf@ornl.gov
+ */
+
+
 (function ($) {
 
 	AjaxSolr.FacetSideBarWidget = AjaxSolr.AbstractFacetWidget.extend({
@@ -76,188 +136,14 @@
 				
 	     	   	Manager.doRequest(0);
 				
-				
 			});
-	    	
-            
 	    	
 		}
 	
 	});
 
-
-	
-	
-	
-	
-	
 	$(document).ready( function() {
-		
-
 		$.fx.speeds._default = 1000;
-		
-		
-		
 	});
 	
 }(jQuery));
-
-
-
-
-
-
-/* other facet browsers
- * implementations here
- * 
- * 
-if($( "#facetTemplate2").html() != null) {
-
-	$("#facetList2").empty();
-	$( "#facetTemplate2").tmpl(facet_arr, {
-		
-    })
-	.appendTo("#facetList2")
-	.find( "a.showFacetValues2" ).click(function() {
-        var selectedItem = $.tmplItem(this);
-        $('li#' + selectedItem.data.Facet_name.toString() + '_facet_container_class2').toggle();
-       
-		});
-	
-}
-
-$("#facetList3").empty();
-
-
-$( "#facetTemplate3").tmpl(facet_arr, {
-	
-})
-.appendTo("#facetList3")
-.find( "a.showFacetValues3" ).click(function() {
-    var selectedItem = $.tmplItem(this);
-    $('li#' + selectedItem.data.Facet_name.toString() + '_facet_container_class3').toggle();
-    
-	});
-*/
-
-
-/*
-if($( "#facetTemplate4").html() != null) {
-
-	$("#facetList4").empty();
-    
-	
-	$( "#facetTemplate4").tmpl(facet_arr, {
-		
-    })
-	.appendTo("#facetList4")
-	.find( "a.showFacetValues4" ).click(function() {
-        var selectedItem = $.tmplItem(this);
-        
-		});
-}
-*/
-
-
-
-/*
-// main vertical scroll
-$("#mainMenu").scrollable({
- 
-	
-	// basic settings
-	vertical: true,
-	
-	
-	// up/down keys will always control this scrollable
-	keyboard: 'static',
-	
-	// assign left/right keys to the actively viewed scrollable
-	onSeek: function(event, i) {
-		//alert('onseek');
-		horizontal.eq(i).data("scrollable").focus();
-	}
-	
- 
-// main navigator (thumbnail images)
-})
-.navigator("#mainMenu_navi");
-
-// horizontal scrollables. each one is circular and has its own navigator instance
-var horizontal = $(".scrollable").scrollable({ circular: true }).navigator(".navi");
- 
-
-// when page loads setup keyboard focus on the first horzontal scrollable
-if(horizontal.eq(0) == null) {
-	horizontal.eq(0).data("scrollable").focus();
-}
-
-$('body').click(function() {
-	 //Hide the menus if visible
-	 $('#mainMenu').hide();
-});
-	
-$('#mainMenu_navi').click(function(){
-    event.stopPropagation();
-	//$('#main').css('margin-top','0px');
-	$('#mainMenu').show();
-});	
-
-$('li#facet_project').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','0px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_model').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','30px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_experiment').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','60px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_frequency').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','90px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_realm').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','120px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_instrument').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','150px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_variable').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','180px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_cf_variable').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','210px');
-	$('#mainMenu').show();
-});
-
-$('li#facet_gcmd_variable').click(function(event) {
-	event.stopPropagation();
-	$('#mainMenu').css('margin-top','240px');
-	$('#mainMenu').show();
-});
-
-$('div#mainMenuPages').click(function(event) {
-	event.stopPropagation();
-});
-*/

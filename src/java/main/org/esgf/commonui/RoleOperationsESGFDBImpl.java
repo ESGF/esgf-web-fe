1a157f00db1b1ea5487d90f125ed4faf3fd33d4b
John Harney <harneyjf@ornl.gov>
2011-08-01 11:10:42 -0400

    Added stubs for implementation of the operations interfaces for interaction with the esgf security db

---
+++ src/java/main/org/esgf/commonui/RoleOperationsESGFDBImpl.java
@@ -0,0 +1,25 @@
+package org.esgf.commonui;
+
+import org.esgf.adminui.Role;
+
+public class RoleOperationsESGFDBImpl implements RoleOperationsInterface {
+
+    @Override
+    public Role getRoleForUserInGroup(String userId, String groupId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Role getRoleObjectFromRoleId(String roleId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getRoleIdFromUserName(String roleName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

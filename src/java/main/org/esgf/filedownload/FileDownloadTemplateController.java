ff60c50008665158c27977b80aa5b0004eb1a6a3
Feiyi Wang <fwang2@ornl.gov>
2011-08-11 15:44:00 -0400

    fix bug on download file number

--- src/java/main/org/esgf/filedownload/FileDownloadTemplateController.java
+++ src/java/main/org/esgf/filedownload/FileDownloadTemplateController.java
@@ -95,11 +95,13 @@ public class FileDownloadTemplateController {
     private final static Logger LOG = Logger.getLogger(FileDownloadTemplateController.class);
 
     //right now the prefix for the solr query is hard coded
-    private final static String filePrefix="q=*%3A*&json.nl=map&fq=type%3AFile&fq=parent_id:"; 
-    
+    //the max rows to be returned is best configurable or read from props file
+    private final static String ROWS="1000";
+    private final static String filePrefix="q=*%3A*&json.nl=map&fq=type%3AFile&rows="+ROWS+"&fq=parent_id:";
+
     //debug flag
     private final static boolean debugFlag = false;
-    
+
     @RequestMapping(method=RequestMethod.GET)
     public @ResponseBody String doGet(HttpServletRequest request, HttpServletResponse response) throws JSONException {
         LOG.debug("doGet");
@@ -115,7 +117,7 @@ public class FileDownloadTemplateController {
     /*
      * Conversion
      */
-    
+
     //responsebody looks like this
     //  <responseHeader>
     //  </responseHeader>
@@ -128,9 +130,9 @@ public class FileDownloadTemplateController {
     //    <size>
     //   </doc>
     //  </response>
-    
+
     //----->
-    
+
     //convert response body to the following format
     //<doc>
     //    <file*>
@@ -143,12 +145,12 @@ public class FileDownloadTemplateController {
     //      </services>
     //    </file>
     //<doc>
-    
-    
+
+
     private String convertTemplateFormat(HttpServletRequest request, HttpServletResponse response) throws JSONException {
 
         String[] names = request.getParameterValues("id[]");
-        
+
         String id = "";
         String responseBody = "";
         JSONObject responseBodyJSON = null;
@@ -156,13 +158,13 @@ public class FileDownloadTemplateController {
 
         SAXBuilder builder = new SAXBuilder();
         Document document = null;
-        
+
         document = new Document(new Element("response"));
         if(names != null) {
-            
+
           //traverse all the dataset ids given by the array
             for(int i=0;i<names.length;i++) {
-                
+
                 id = names[i];
                 responseBody = getResponseBody(id);
                 responseBodyJSON = new JSONObject(responseBody);
@@ -170,43 +172,43 @@ public class FileDownloadTemplateController {
                 //get the different json texts here
                 JSONObject responseJSON = new JSONObject(responseBodyJSON.get("response").toString());
                 JSONArray docsJSON = responseJSON.getJSONArray("docs");
-                
+
                 try{
                 //  create <doc> element
                     Element docEl = new Element("doc");
-                    
+
                 //  create doc/dataset_id element
                     Element dataset_idEl = new Element("dataset_id");
                     dataset_idEl.addContent(id);
                     docEl.addContent(dataset_idEl);
-         
-                    
+
+
                 //  for each file found
                     for(int j=0;j<docsJSON.length();j++) {
                         JSONObject docJSON = new JSONObject(docsJSON.get(j).toString());
                         Element fileEl = createFileElement(docJSON);
                         docEl.addContent(fileEl);
                     }
-                    
-                    
+
+
                     document.getRootElement().addContent(docEl);
                 }
                 catch(Exception e) {
                     LOG.debug("\nJSON errors - investigate line 167\n");
                 }
-                
+
             }
         }
-        
-        
+
+
         XMLOutputter outputter = new XMLOutputter();
         xmlOutput = outputter.outputString(document.getRootElement());
 
         if(debugFlag) {
             LOG.debug("xmlOutput:\n " + xmlOutput);
         }
-       
-        
+
+
         JSONObject returnJSON = XML.toJSONObject(xmlOutput);
 
         String jsonContent = returnJSON.toString();
@@ -216,27 +218,27 @@ public class FileDownloadTemplateController {
         return jsonContent;
         //return jo.toString();
     }
-    
+
     private static String getResponseBody(String id) {
 
         String responseBody = null;
-        
+
         String newURL = filePrefix + id;
 
 
         // create an http client
         HttpClient client = new HttpClient();
-        
-        
+
+
         //String urlString = solrURL + "select?" + queryString + "&wt=json";
         String urlString = solrURL + "select?" + newURL + "&wt=json";
-        
+
         if(debugFlag) {
             LOG.debug("urlString: " + urlString);
         }
-        
+
         GetMethod method = new GetMethod(urlString);
-        
+
         method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
                 new DefaultHttpMethodRetryHandler(3, false));
         try {
@@ -261,20 +263,20 @@ public class FileDownloadTemplateController {
         } finally {
             method.releaseConnection();
         }
-        
+
         if(responseBody != null) {
             if(debugFlag) {
                 LOG.debug("responseBody: " + responseBody);
             }
         }
-        
+
         return responseBody;
     }
 
     public static Element createFileElement(JSONObject docJSON) throws JSONException {
         // create <file> element
         Element fileEl = new Element("file");
-        
+
         // create file/file_id element
         Element file_idEl = new Element("file_id");
         file_idEl.addContent(docJSON.get("id").toString());
@@ -284,23 +286,23 @@ public class FileDownloadTemplateController {
         Element titleEl = new Element("title");
         titleEl.addContent(docJSON.get("title").toString());
         fileEl.addContent(titleEl);
-        
+
         // create file/file_size element
         Element sizeEl = new Element("size");
         sizeEl.addContent(docJSON.get("size").toString());
         fileEl.addContent(sizeEl);
-     
+
         // create file/url element
         Element urlEl = new Element("url");
         JSONArray urlsJSON = docJSON.getJSONArray("url");
-        
+
         urlEl.addContent(urlsJSON.get(0).toString());
         fileEl.addContent(urlEl);
-     
-        
+
+
         // create file/services element
         Element servicesEl = new Element("services");
-        
+
         JSONArray docsJSON = docJSON.getJSONArray("service");
         for(int i=0;i<docsJSON.length();i++) {
             Element serviceEl = new Element("service");
@@ -312,8 +314,8 @@ public class FileDownloadTemplateController {
             servicesEl.addContent(serviceEl);
         }
         fileEl.addContent(servicesEl);
-        
+
         return fileEl;
     }
-   
+
 }

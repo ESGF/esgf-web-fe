39896a0fefa0876a2ca71de1c991390d399a37a4
root <root@spock.ornl.gov>
2011-09-12 18:57:21 -0400

    Added extended wget script generator.
    Also included in this commit:
    - file name strings in the data cart are now longer and more legible than before
    - there are now 3 separate versions of the wget script.  Added a toggle flag called "security" that tells the backend which version of the script to give to the end user.  V3 is the latest version and the default.`

--- web/scripts/esgf/esgf-download.js
+++ web/scripts/esgf/esgf-download.js
@@ -1,11 +1,11 @@
 
 /*****************************************************************************
- * Copyright © 2011 , UT-Battelle, LLC All rights reserved
+ * Copyright ï¿½ 2011 , UT-Battelle, LLC All rights reserved
  *
  * OPEN SOURCE LICENSE
  *
  * Subject to the conditions of this License, UT-Battelle, LLC (the
- * ÒLicensorÓ) hereby grants to any person (the ÒLicenseeÓ) obtaining a copy
+ * ï¿½Licensorï¿½) hereby grants to any person (the ï¿½Licenseeï¿½) obtaining a copy
  * of this software and associated documentation files (the "Software"), a
  * perpetual, worldwide, non-exclusive, irrevocable copyright license to use,
  * copy, modify, merge, publish, distribute, and/or sublicense copies of the
@@ -15,7 +15,7 @@
  * grant, copyright and license notices, this list of conditions, and the
  * disclaimer listed below.  Changes or modifications to, or derivative works
  * of the Software must be noted with comments and the contributor and
- * organizationÕs name.  If the Software is protected by a proprietary
+ * organizationï¿½s name.  If the Software is protected by a proprietary
  * trademark owned by Licensor or the Department of Energy, then derivative
  * works of the Software may not be distributed using the trademark without
  * the prior written approval of the trademark owner.
@@ -28,7 +28,7 @@
  * acknowledgment:
  *
  *    "This product includes software produced by UT-Battelle, LLC under
- *    Contract No. DE-AC05-00OR22725 with the Department of Energy.Ó
+ *    Contract No. DE-AC05-00OR22725 with the Department of Energy.ï¿½
  *
  * 4. Licensee is authorized to commercialize its derivative works of the
  * Software.  All derivative works of the Software must include paragraphs 1,
@@ -215,8 +215,8 @@ $(document).ready( function() {
             },
             abbreviate : function (word) {
                 var abbreviation = word;
-                if(word.length > 25) {
-                    abbreviation = word.slice(0,10) + '...' + word.slice(word.length-11,word.length);
+                if(word.length > 50) {
+                    abbreviation = word.slice(0,20) + '...' + word.slice(word.length-21,word.length);
                 }
                 return abbreviation;
             },
@@ -393,7 +393,7 @@ $(document).ready( function() {
             
 
             var url = '/esgf-web-fe/wgetproxy';
-            var security = 'standard';
+            var security = 'wgetv3';
             queryString += '&security=' + security;
             
             //assemble the input fields with the query string

694f8cd4d970e0c0640cbb0c2d7e9df015735b28
John Harney <John@sparta.ornl.gov>
2012-06-21 18:56:30 -0400

    Changed verbiage from transerring to transferring

--- web/scripts/ajax-solr/widgets/datacart/DataCart.js
+++ web/scripts/ajax-solr/widgets/datacart/DataCart.js
@@ -1,10 +1,10 @@
 /*****************************************************************************
- * Copyright © 2011 , UT-Battelle, LLC All rights reserved
+ * Copyright ï¿½ 2011 , UT-Battelle, LLC All rights reserved
  *
  * OPEN SOURCE LICENSE
  *
  * Subject to the conditions of this License, UT-Battelle, LLC (the
- * ÒLicensorÓ) hereby grants to any person (the ÒLicenseeÓ) obtaining a copy
+ * ï¿½Licensorï¿½) hereby grants to any person (the ï¿½Licenseeï¿½) obtaining a copy
  * of this software and associated documentation files (the "Software"), a
  * perpetual, worldwide, non-exclusive, irrevocable copyright license to use,
  * copy, modify, merge, publish, distribute, and/or sublicense copies of the
@@ -14,7 +14,7 @@
  * grant, copyright and license notices, this list of conditions, and the
  * disclaimer listed below.  Changes or modifications to, or derivative works
  * of the Software must be noted with comments and the contributor and
- * organizationÕs name.  If the Software is protected by a proprietary
+ * organizationï¿½s name.  If the Software is protected by a proprietary
  * trademark owned by Licensor or the Department of Energy, then derivative
  * works of the Software may not be distributed using the trademark without
  * the prior written approval of the trademark owner.
@@ -27,7 +27,7 @@
  * acknowledgment:
  *
  *    "This product includes software produced by UT-Battelle, LLC under
- *    Contract No. DE-AC05-00OR22725 with the Department of Energy.Ó
+ *    Contract No. DE-AC05-00OR22725 with the Department of Energy.ï¿½
  *
  * 4. Licensee is authorized to commercialize its derivative works of the
  * Software.  All derivative works of the Software must include paragraphs 1,
@@ -244,7 +244,7 @@ AjaxSolr.DataCartWidget = AjaxSolr.AbstractWidget.extend({
 			datasetList += '<a class="showAllFiles_short" style="cursor:pointer">Expand</a> | ';
 			datasetList += '<span class="wgetAllFiles_short" style="display:none;font-weight:bold;"> Downloading... </span>';
 			datasetList += '<a class="wgetAllFiles_short" style="cursor:pointer"> WGET </a> |';
-			datasetList += '<span class="globusOnlineAllFiles_short" style="display:none;font-weight:bold;"> Transerring to GO page...</span>';
+			datasetList += '<span class="globusOnlineAllFiles_short" style="display:none;font-weight:bold;"> Transferring to GO page...</span>';
 			datasetList += ' <a class="globusOnlineAllFiles_short" style="cursor:pointer">Globus Online</a> |';
 			datasetList += ' <a class="remove_dataset_short" style="cursor:pointer">Remove</a>'; 
 			datasetList += '</td>';	
